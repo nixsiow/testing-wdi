1b0653bbed4e12c959a1914204ac37327ab57d49
Cold Head <certainthings@gmail.com>
2013-07-29 10:53:35 +1000

    add method

--- cucumber/features/calculator.rb
+++ cucumber/features/calculator.rb
@@ -1,2 +1,15 @@
 class Calculator
+  def initialize
+    @input = []
+  end
+
+  def <<(number)
+    @input.push(number)
+  end
+
+  def add
+    result = @input.inject{|sum, x| sum + x}
+    puts result
+    result
+  end
 end
