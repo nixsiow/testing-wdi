721d4d8e7d153a796b441b8c6cc50241d411468d
Cold Head <certainthings@gmail.com>
2013-07-29 12:52:15 +1000

    devise, signup scenario complete

--- cuky/config/environments/development.rb
+++ cuky/config/environments/development.rb
@@ -34,4 +34,7 @@ Cuky::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Per the devise install instructions.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
