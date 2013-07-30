96d3658dc7c6b882666510d4ecd302e0f8833033
Cold Head <certainthings@gmail.com>
2013-07-30 10:47:06 +1000

    Single Table Inheritance

--- rspec/db/schema.rb
+++ rspec/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130730001251) do
+ActiveRecord::Schema.define(:version => 20130730002136) do
 
   create_table "fruits", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "name"
+    t.string   "type"
   end
 
 end
--- rspec/spec/models/fruit_spec.rb
+++ rspec/spec/models/fruit_spec.rb
@@ -14,7 +14,7 @@ describe Fruit do
 
   describe "An apple" do
     before do
-      @apple = Apple.new(:name => 'Apple')
+      @apple = Apple.create(:name => 'Apple')
     end
 
     it "should not be squishy" do
@@ -23,11 +23,32 @@ describe Fruit do
       @apple.squishy?.should be_false
       @apple.squishy?.should eq(false)
     end
+
+    it "should remember what class it is using Single Table Inheritance (STI)" do
+      apple = Fruit.find(@apple.id)
+
+      # Test we can find our apple.
+      apple.should_not == nil
+      apple.should_not be_nil
+      apple.should_not eq(nil)
+
+      # Test it's the right kind of fruit, via type column.
+      apple.class.should == Apple
+      apple.class.should eq(Apple)
+
+      # Test it's our ACTUAL apple.
+      apple.should == @apple
+      apple.should eq(@apple)
+
+      # Test inheritance.
+      apple.is_a?(Fruit).should be_true
+      apple.class.ancestors.should include Fruit
+    end
   end
 
   describe "A pear" do
     before do
-      @pear = Pear.new(:name => 'Pear')
+      @pear = Pear.create(:name => 'Pear')
     end
 
     it "should be kinda squishy" do
@@ -35,6 +56,22 @@ describe Fruit do
       @pear.squishy?.should == true
       @pear.squishy?.should be_true
       @pear.squishy?.should eq(true)
+    end
+
+    it "should remember what class it is using Single Table Inheritance (STI)" do
+      pear = Fruit.find(@pear.id)
+      pear.should_not == nil
+      pear.should_not be_nil
+      pear.should_not eq(nil)
+
+      pear.class.should == Pear
+      pear.class.should eq(Pear)
+
+      pear.should == @pear
+      pear.should eq(@pear)
+
+      pear.is_a?(Fruit).should be_true
+      pear.class.ancestors.should include Fruit
     end
   end
 end
