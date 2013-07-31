0d46812a05f4bf35f7f0b92eda17323fd9e08358
Cold Head <certainthings@gmail.com>
2013-07-31 12:49:54 +1000

    fruits instance variable in index aciton

--- rspec/spec/controllers/fruits_controller_spec.rb
+++ rspec/spec/controllers/fruits_controller_spec.rb
@@ -15,6 +15,17 @@ describe FruitsController do
         expect(response).to be_success
         expect(response.status).to eq(200)
       end
+
+      it "should give the fruits in an instance variable in reverse order" do
+        expect(assigns(:fruits)).to be
+        expect(assigns(:fruits).length).to eq(3)
+        expect(assigns(:fruits).first.class).to eq(Fruit)
+        expect(assigns(:fruits).first.name).to eq("Fruit number 2")
+      end
+
+      it "should render the index template" do
+        expect(response).to render_template("index")
+      end
     end
   end
 end
