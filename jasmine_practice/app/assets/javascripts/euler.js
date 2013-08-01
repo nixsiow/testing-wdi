4f30aeaa6638a058eb71889742f6d4e13b60e3d2
Cold Head <certainthings@gmail.com>
2013-08-01 12:00:06 +1000

    as a while() loop

--- jasmine_practice/app/assets/javascripts/euler.js
+++ jasmine_practice/app/assets/javascripts/euler.js
@@ -1,6 +1,15 @@
 function getMulThreeFive(upto) {
   var result = [];
 
+  var i = 1;
+  while (i < upto) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      result.push(i);
+    }
+    i++;
+  }
+  return result;
+/*
   // Consider every number between 1 and upto.
   for (var i = 1; i < upto; i++) {
     // Add multiples of 3 or 5 to our results.
@@ -12,4 +21,5 @@ function getMulThreeFive(upto) {
   }
 
   return result;
+*/
 }
